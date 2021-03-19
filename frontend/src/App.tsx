5244d98a47598b591c93fa03fc21aa172863b4b7
marc <schweringmarc01@gmail.com>
2021-03-19 21:35:40 +0100

    switched responsive context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -55,68 +55,61 @@ const AppBar = (props: any) => (
 );
 
 function AppContent(): JSX.Element {
-  const [showSidebar, setShowSidebar] = React.useState(false);
+  const [showSidebar, setShowSidebar] = React.useState(true);
   const [loadTestPkl, { data }] = useTestPkl();
+  const size = React.useContext(ResponsiveContext);
 
   return (
-    <ResponsiveContext.Consumer>
-      {(size) => (
-        <Box fill>
-          <AppBar>
-            <Heading level="3" margin="none">
-              My App
-            </Heading>
-            <Button
-              icon={<Notification />}
-              onClick={() => setShowSidebar(!showSidebar)}
-            />
-          </AppBar>
-          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-            <Box flex align="center" justify="center">
-              app body
-            </Box>
-            {!showSidebar || size !== "small" ? (
-              <Collapsible direction="horizontal" open={showSidebar}>
-                <Box
-                  flex
-                  width="medium"
-                  background="light-2"
-                  elevation="small"
-                  align="center"
-                  justify="center"
-                >
-                  sidebar
-                  <Button
-                    primary
-                    label="get data"
-                    onClick={() => loadTestPkl()}
-                  />
-                  <Text>N Records: {data?.testPkl?.length}</Text>
-                </Box>
-              </Collapsible>
-            ) : (
-              <Layer>
-                <Box
-                  background="light-2"
-                  tag="header"
-                  justify="end"
-                  align="center"
-                  direction="row"
-                >
-                  <Button
-                    icon={<FormClose />}
-                    onClick={() => setShowSidebar(false)}
-                  />
-                </Box>
-                <Box fill background="light-2" align="center" justify="center">
-                  sidebar
-                </Box>
-              </Layer>
-            )}
-          </Box>
+    <Box fill>
+      <AppBar>
+        <Heading level="3" margin="none">
+          My App
+        </Heading>
+        <Button
+          icon={<Notification />}
+          onClick={() => setShowSidebar(!showSidebar)}
+        />
+      </AppBar>
+      <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+        <Box flex align="center" justify="center">
+          app body
         </Box>
-      )}
-    </ResponsiveContext.Consumer>
+        {!showSidebar || size !== "small" ? (
+          <Collapsible direction="horizontal" open={showSidebar}>
+            <Box
+              flex
+              width="medium"
+              background="light-2"
+              elevation="small"
+              align="center"
+              justify="center"
+            >
+              sidebar
+              <Button primary label="get data" onClick={() => loadTestPkl()} />
+              <Text>N Records: {data?.testPkl?.length}</Text>
+            </Box>
+          </Collapsible>
+        ) : (
+          <Layer>
+            <Box
+              background="light-2"
+              tag="header"
+              justify="end"
+              align="center"
+              direction="row"
+            >
+              <Button
+                icon={<FormClose />}
+                onClick={() => setShowSidebar(false)}
+              />
+            </Box>
+            <Box fill background="light-2" align="center" justify="center">
+              sidebar
+            </Box>
+          </Layer>
+        )}
+      </Box>
+    </Box>
   );
 }
 
