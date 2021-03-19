e1618d205af78cce5d35404d9ae63f2fa3284377
marc <schweringmarc01@gmail.com>
2021-03-19 21:32:40 +0100

    added Apollo with initial test query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import {
   Box,
   Button,
   Heading,
@@ -7,8 +13,19 @@ import {
   Grommet,
   ResponsiveContext,
   Layer,
+  Text,
 } from "grommet";
 import { Notification, FormClose } from "grommet-icons";
+import { useTestPkl } from "./queries";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 const theme = {
   global: {
@@ -37,69 +54,79 @@ const AppBar = (props: any) => (
   />
 );
 
-function App() {
+function AppContent(): JSX.Element {
   const [showSidebar, setShowSidebar] = React.useState(false);
+  const [loadTestPkl, { data }] = useTestPkl();
 
   return (
-    <Grommet theme={theme} full>
-      <ResponsiveContext.Consumer>
-        {(size) => (
-          <Box fill>
-            <AppBar>
-              <Heading level="3" margin="none">
-                My App
-              </Heading>
-              <Button
-                icon={<Notification />}
-                onClick={() => setShowSidebar(!showSidebar)}
-              />
-            </AppBar>
-            <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
-              <Box flex align="center" justify="center">
-                app body
-              </Box>
-              {!showSidebar || size !== "small" ? (
-                <Collapsible direction="horizontal" open={showSidebar}>
-                  <Box
-                    flex
-                    width="medium"
-                    background="light-2"
-                    elevation="small"
-                    align="center"
-                    justify="center"
-                  >
-                    sidebar
-                  </Box>
-                </Collapsible>
-              ) : (
-                <Layer>
-                  <Box
-                    background="light-2"
-                    tag="header"
-                    justify="end"
-                    align="center"
-                    direction="row"
-                  >
-                    <Button
-                      icon={<FormClose />}
-                      onClick={() => setShowSidebar(false)}
-                    />
-                  </Box>
-                  <Box
-                    fill
-                    background="light-2"
-                    align="center"
-                    justify="center"
-                  >
-                    sidebar
-                  </Box>
-                </Layer>
-              )}
+    <ResponsiveContext.Consumer>
+      {(size) => (
+        <Box fill>
+          <AppBar>
+            <Heading level="3" margin="none">
+              My App
+            </Heading>
+            <Button
+              icon={<Notification />}
+              onClick={() => setShowSidebar(!showSidebar)}
+            />
+          </AppBar>
+          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+            <Box flex align="center" justify="center">
+              app body
             </Box>
+            {!showSidebar || size !== "small" ? (
+              <Collapsible direction="horizontal" open={showSidebar}>
+                <Box
+                  flex
+                  width="medium"
+                  background="light-2"
+                  elevation="small"
+                  align="center"
+                  justify="center"
+                >
+                  sidebar
+                  <Button
+                    primary
+                    label="get data"
+                    onClick={() => loadTestPkl()}
+                  />
+                  <Text>N Records: {data?.testPkl?.length}</Text>
+                </Box>
+              </Collapsible>
+            ) : (
+              <Layer>
+                <Box
+                  background="light-2"
+                  tag="header"
+                  justify="end"
+                  align="center"
+                  direction="row"
+                >
+                  <Button
+                    icon={<FormClose />}
+                    onClick={() => setShowSidebar(false)}
+                  />
+                </Box>
+                <Box fill background="light-2" align="center" justify="center">
+                  sidebar
+                </Box>
+              </Layer>
+            )}
           </Box>
-        )}
-      </ResponsiveContext.Consumer>
-    </Grommet>
+        </Box>
+      )}
+    </ResponsiveContext.Consumer>
+  );
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Grommet theme={theme} full>
+        <AppContent />
+      </Grommet>
+    </ApolloProvider>
   );
 }
 
---
+++ frontend/src/queries.tsx
@@ -0,0 +1,65 @@
+import { gql, useLazyQuery, ApolloError } from "@apollo/client";
+
+/* below from https://www.graphql-code-generator.com/ */
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Query = {
+  __typename?: "Query";
+  meta: Meta;
+  testJson: Scalars["Int"];
+  testPkl?: Maybe<Array<Maybe<AvgWinds>>>;
+};
+
+export type Meta = {
+  __typename?: "Meta";
+  ciPipelineId: Scalars["String"];
+  buildDate: Scalars["String"];
+};
+
+export type AvgWinds = {
+  __typename?: "AvgWinds";
+  dir: Scalars["String"];
+  vel: Scalars["Int"];
+  velName: Scalars["String"];
+  count: Scalars["Int"];
+};
+/* above from https://www.graphql-code-generator.com/ */
+
+const QUERY = gql`
+  query TestPkl {
+    testPkl {
+      dir
+      vel
+      velName
+      count
+    }
+  }
+`;
+
+export function useTestPkl(): [
+  () => void,
+  {
+    data?: Query;
+    loading: boolean;
+    error?: ApolloError;
+  }
+] {
+  const [loadTestPkl, { data, loading, error }] = useLazyQuery<Query>(QUERY);
+
+  return [loadTestPkl, { data, loading, error }];
+}
