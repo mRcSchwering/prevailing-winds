884d488a7fe7e8cd90a02eda4acf3443d78307a5
marc <schweringmarc01@gmail.com>
2021-08-16 20:17:04 +0200

    added env.yml to prep

--- prep/s4_upload_objs.py
+++ prep/s4_upload_objs.py
@@ -1,112 +1,76 @@
 """
-Write all objects to S3:
-- key: <year>/<month>/avgwinds_<lat>;<lng>.pkl
-- file: keys and counts of winds where keys are (<direction>, <velocity>)
+Write all objects to s3
+
+One object for every position x month x time range => ~20M objects.
+
+Vanilla from home 1 month, 100 records: 18s => 20 days
+Subroutines from home 1 month, 100 records: 7s => 8 days
+
+
+    python s4_upload_objs.py test
+    python s4_upload_objs.py
+
 """
 from pathlib import Path
-from typing import List
 import sys
-import pickle
-import numpy as np  # type: ignore
-import pandas as pd  # type: ignore
-from src.util import read_parquet, bin_rain, count_rain_bins
-import src.s3 as s3
+import eventlet  # type: ignore
 
-TIME_RANGES = {"2020": [2020], "2016-2020": [2016, 2017, 2018]}
+eventlet.monkey_patch()
+from src.util import read_parquet
+import src.s3 as s3
+import time
+
+TIME_RANGES = ["2020", "2016-2020"]
 MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-DATA_DIR = Path("/media/marc/Elements/copernicus/tmp")
+PREFIX = "v2"
+DATA_DIR = Path("data/tmp")
 IS_TEST = "test" in sys.argv[1:]
 
-
-def create_prec_objs(years: List[int], label: str):
-    print(f"\nCreating precipitation {label}...")
-    for month in MONTHS:
-        df = read_parquet(DATA_DIR / f"s2_prec_{years[0]}-{month}.pq")
-        index = df.index.copy()
-
-        bins = []
-        for year in years:
-            print(f"Creating precipitation {label}...")
-            df = read_parquet(DATA_DIR / f"s2_prec_{year}-{month}.pq")
-            assert df.index.equals(index)
-            bins.append(bin_rain(df * 1000))
-
-        counts = count_rain_bins(np.concatenate(bins, axis=1))
-        df = pd.DataFrame(counts, index=df.index)
-        df.drop(columns=0, inplace=True)
-
-        for (lat, lng), row in df.iterrows():
-            s3.write_wind_obj(
-                obj=row.to_dict(),
-                year=label,
-                month=month,
-                lat=lat,
-                lng=lng,
-                prefix="v2",
-            )
-
-
-single_years = [2020]
-time_ranges = {"2016-2020": (2016, 2020)}
-
-
-def upload_single_year(year: int):
-    print(f"\nStarting year {year}...")
-
-    with open(f"data/monthly_counts_{year}.pkl", "rb") as fh:
-        data = pickle.load(fh)
-
-    for month in data[year].keys():
-        print(f"Starting {year}/{month}...")
-
-        for lat, lng in data[year][month]:
-            s3.write_wind_obj(
-                obj=data[year][month][(lat, lng)],
-                year=year,
-                month=month,
-                lat=lat,
-                lng=lng,
-            )
-
-
-def upload_time_range(from_year: int, to_year: int, name: str):
-    print(f"\nStarting timerange {name}...")
-    full_time_range = list(range(from_year, to_year + 1))
-
-    print(f"Aggregating all years...")
-    res = {}  # type: ignore
-    for year in full_time_range:
-        with open(f"data/monthly_counts_{year}.pkl", "rb") as fh:
-            data = pickle.load(fh)[year]
-
-        for month in data:
-            if month not in res:
-                res[month] = {}
-
-            for pos in data[month]:
-                if pos not in res[month]:
-                    res[month][pos] = {}
-
-                for key, count in data[month][pos].items():
-                    if key not in res[month][pos]:
-                        res[month][pos][key] = count
-                    else:
-                        res[month][pos][key] = res[month][pos][key] + count
-
-    print("Uploading objects...")
-    for month in res.keys():
-        print(f"Starting {name}/{month}...")
-        for lat, lng in res[month]:
-            s3.write_wind_obj(
-                obj=res[month][(lat, lng)], year=name, month=month, lat=lat, lng=lng,
-            )
+# TODO: check https://stackoverflow.com/questions/27478568/how-to-upload-small-files-to-amazon-s3-efficiently-in-python
+# mit https://eventlet.net/
 
 
 if __name__ == "__main__":
-    for year in single_years:
-        # upload_single_year(year)  # TODO: put back in
-        pass
-    for key, years in time_ranges.items():
-        upload_time_range(*years, name=key)
+    t0 = time.time()
+    for time_range in TIME_RANGES:
+        for month in MONTHS[:1] if IS_TEST else MONTHS:
+            print(f"Processing {time_range}/{month}...")
 
+            prec = read_parquet(DATA_DIR / f"s3_prec_{time_range}_{month}.pq")
+            prec.columns = [str(d) for d in prec.columns]
+            tmps = read_parquet(DATA_DIR / f"s3_tmps_{time_range}_{month}.pq")
+            tmps.rename(
+                columns={
+                    "high_mean": "highMean",
+                    "high_std": "highStd",
+                    "low_mean": "lowMean",
+                    "low_std": "lowStd",
+                },
+                inplace=True,
+            )
+            winds = read_parquet(DATA_DIR / f"s3_winds_{time_range}_{month}.pq")
+            winds.columns = [tuple(d.split("|")) for d in winds.columns]
+
+            assert prec.index.equals(tmps.index)
+            assert tmps.index.equals(winds.index)
+            n = len(prec.index)
+
+            pool = eventlet.GreenPool()
+            for i, (lng, lat) in enumerate(prec.index):
+                if i % 40000 == 0:
+                    print(f"...{i/n*100:.0f}% done...")
+                if IS_TEST and i > 100:
+                    break
+
+                key = f"{PREFIX}/{time_range}/{month}/{lat:.2f}/{lng:.2f}/data.pkl"
+                data = {
+                    "prec": prec.loc[(lng, lat)].to_dict(),
+                    "tmps": tmps.loc[(lng, lat)].to_dict(),
+                    "winds": winds.loc[(lng, lat)].to_dict(),
+                }
+                # s3.put_obj(key=key, obj=data)
+                pool.spawn_n(s3.put_obj, key, data)
+            pool.waitall()
+
+    print(time.time() - t0)
     print("\ndone")
--- prep/src/s3.py
+++ prep/src/s3.py
@@ -1,14 +1,13 @@
 """
 Writing to S3
 """
-from typing import Optional, Union, Any
+from typing import Any
 import pickle
 import boto3  # type: ignore
 
 AWS_REGION = "eu-central-1"
 CONTENT_BUCKET_NAME = "prevailing-winds-data"
 CLIENT = boto3.client("s3", region_name=AWS_REGION)
-RESOURCE = boto3.resource("s3", region_name=AWS_REGION)
 
 
 def get_obj(key: str) -> Any:
@@ -18,6 +17,6 @@ def get_obj(key: str) -> Any:
 
 
 def put_obj(key: str, obj: Any):
-    res = RESOURCE.Object(CONTENT_BUCKET_NAME, key).put(Body=pickle.dumps(obj))
+    res = CLIENT.put_object(Bucket=CONTENT_BUCKET_NAME, Key=key, Body=pickle.dumps(obj))
     assert res["ResponseMetadata"]["HTTPStatusCode"] == 200
 
