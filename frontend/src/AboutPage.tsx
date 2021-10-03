d8a8381c7b1b7ea631210092c9e77e3093d22662
marc <schweringmarc01@gmail.com>
2021-10-03 19:03:44 +0200

    updated about page

--- frontend/src/AboutPage.tsx
+++ frontend/src/AboutPage.tsx
@@ -36,8 +36,8 @@ const era5Link = (
 
 const dataprepLink = (
   <Anchor
-    href="https://github.com/mRcSchwering/prevailing-winds/blob/main/prep/2_calc_avg_winds.py"
-    label="prep/2_calc_avg_winds.py (github.com)"
+    href="https://github.com/mRcSchwering/prevailing-winds/tree/main/prep"
+    label="prep/ (github.com)"
     target="_blank"
   />
 );
@@ -52,7 +52,7 @@ export default function AboutPage(): JSX.Element {
           There are many great apps like {windyLink} that show you the current
           winds (and more) with short-term predictions. However, I didn't really
           find anything with historic wind data for long-term planning,
-          <i> e.g.</i> when planning a holiday or passage. So I made this app.
+          <i> e.g.</i> when planning a holiday or a passage. So I made this app.
           The code is all on {repoLink}. The data is derived from{" "}
           {copernicusLink} datasets.
         </Paragraph>
@@ -63,23 +63,14 @@ export default function AboutPage(): JSX.Element {
         </Paragraph>
         <Heading level="4">Data</Heading>
         <Paragraph>
-          I used {era5Link} as a basis for calculating wind strengths and
-          directions. This dataset is a ECMWF reanalysis which holds hourly data
-          of a lot of variables like wind, pressure, temperature. Winds were
-          regridded to a regular lat-lon grid of 0.25 degrees. To reduce the
-          download sizes I only took every 3rd hour (00:00, 03:00, 06:00, ...)
-          and exluded 20° from the poles (70°S to 70°N).
-        </Paragraph>
-        <Paragraph>
-          The data is prepared by averaging the wind vectors over certain areas.
-          I chose to average over the area of 1°N and 1°E at every full degree
-          lat-lng. With the lat-lon regridded data points every 0.25 degree this
-          is an average over 16 data points at each full degree lat-lng. At the
-          equator this area spans 3,600M<sup>2</sup>. Going further to either
-          pole this area gets smaller. Then, I used the Beaufort scale for
-          binning wind velocities and the 16 traditional compass rose bearings
-          (N, NNE, NE, ...) for binning wind directions. If you are interested
-          in the details, this data preparation is done in {dataprepLink}.
+          I used {era5Link} as a basis for calculating weather data. This
+          dataset is a ECMWF reanalysis which holds hourly data of a lot of
+          variables like wind, pressure, temperature. Winds were regridded to a
+          regular lat-lon grid of 0.25 degrees. To reduce the download sizes I
+          only took every 3rd hour (00:00, 03:00, 06:00, ...) of years 2016-2020
+          and exluded 20° from the poles (70°S to 70°N). For details about data
+          preparation see {dataprepLink}. When you select an area on the map you
+          always see aggregated data for this particular area, month, and years.
         </Paragraph>
       </Box>
     </Box>
